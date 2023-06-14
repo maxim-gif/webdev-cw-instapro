ae6687d8dbff875f11cdfbe07d0b4511ba399a1a
Maxim <maximagaffonnovv@gmail.com>
2023-06-14 17:43:59 +0300

    добавил страницу добавления поста

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,15 +1,28 @@
+import { renderHeaderComponent } from "./header-component.js";
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+      <h1 style="text-align: center;">Добавить пост</h1>
+      <label class="file-upload-label secondary-button">
+        <input type="file" class="file-upload-input" style="display:none">
+        Выберите фото
+    </label>
+    <div class="page" style="padding-top: 10px; padding-bottom: 5px;">
+    <span><b>Опишите фотографию:</b></span>
+    <textarea class="dependens" name="" id=""></textarea>
+    </div>
+    <button class="button" id="add-button" style="width: 100%;">Добавить</button>
     </div>
   `;
 
     appEl.innerHTML = appHtml;
+
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
@@ -18,6 +31,7 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
       });
     });
   };
+
 
   render();
 }
